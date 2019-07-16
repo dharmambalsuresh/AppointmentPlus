313e833c799cb00bdfc8c57733961edb9e5da555
Abhinandan Walia <ab403576@dal.ca>
2019-07-16 20:28:48 -0300

    Email notification on successfull registration

--- server/routes/doctor/registerUser.js
+++ server/routes/doctor/registerUser.js
@@ -3,9 +3,9 @@
 var con = require('../../database/DBConfig').con;
 var express = require('express');
 var router = express.Router();
-//var nodemailer = require('nodemailer');
-//const { google } = require("googleapis");
-//const OAuth2 = google.auth.OAuth2;
+var nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 router.post('/createDoctorUser', function(req, res, next) {
      licenseNumber = req.body.licenseNumber;
@@ -38,41 +38,41 @@ router.post('/createDoctorUser', function(req, res, next) {
      //console.log(result);
 
 // send Email in successful registration
-//      async function sendEmail() {
-//         const oauth2Client = new OAuth2(
-//          "736535151881-7tfovvs8na5fl3taah3he9l55g22pcj6.apps.googleusercontent.com", // ClientID
-//          "ytUA8zh0XzYFjMHR08NEANq1", // Client Secret
-//          "https://developers.google.com/oauthplayground" // Redirect URL
-//     );
-//     oauth2Client.setCredentials({
-//      refresh_token: "1/bqPhjitXxfOV9UTj01ow8ixWgo3QW5jRInxLfH4OfDE"
-//    });
-//    const tokens = await oauth2Client.refreshAccessToken()
-//    const accessToken = tokens.credentials.access_token //expires after 3600 sec
+     async function sendEmail() {
+        const oauth2Client = new OAuth2(
+         "736535151881-7tfovvs8na5fl3taah3he9l55g22pcj6.apps.googleusercontent.com", // ClientID
+         "ytUA8zh0XzYFjMHR08NEANq1", // Client Secret
+         "https://developers.google.com/oauthplayground" // Redirect URL
+    );
+    oauth2Client.setCredentials({
+     refresh_token: "1/wGdWYwJ6-BzCsFpAwScJlAFqHHouNF8SkSzAd8Cg2jY"
+   });
+   const tokens = await oauth2Client.refreshAccessToken()
+   const accessToken = tokens.credentials.access_token //expires after 3600 sec and fetched for new automatically
    
-//    const smtpTransport = nodemailer.createTransport({
-//      service: "gmail",
-//      auth: {
-//           type: "OAuth2",
-//           user: "abhinandanwalia14@gmail.com", 
-//           clientId: "736535151881-7tfovvs8na5fl3taah3he9l55g22pcj6.apps.googleusercontent.com",
-//           clientSecret: "ytUA8zh0XzYFjMHR08NEANq1",
-//           refreshToken: "1/bqPhjitXxfOV9UTj01ow8ixWgo3QW5jRInxLfH4OfDE",
-//           accessToken: accessToken
-//      }
-//    });
-//    const mailOptions = {
-//      from: "abhinandanwalia14@gmail.com",
-//      to: email,
-//      subject: "Registration Successful",
-//      generateTextFromHTML: true,
-//      html: "<h3> Registration was successful..!! </h3>" 
-//    };
-//    smtpTransport.sendMail(mailOptions, (error, response) => {
-//      error ? console.log(error) : console.log(response);
-//      smtpTransport.close();
-//    });} // That last brace is to close off our async function
-//    sendEmail();
+   const smtpTransport = nodemailer.createTransport({
+     service: "gmail",
+     auth: {
+          type: "OAuth2",
+          user: "abhinandanwalia14@gmail.com", 
+          clientId: "736535151881-7tfovvs8na5fl3taah3he9l55g22pcj6.apps.googleusercontent.com",
+          clientSecret: "ytUA8zh0XzYFjMHR08NEANq1",
+          refreshToken: "1/wGdWYwJ6-BzCsFpAwScJlAFqHHouNF8SkSzAd8Cg2jY",
+          accessToken: accessToken
+     }
+   });
+   const mailOptions = {
+     from: "abhinandanwalia14@gmail.com",
+     to: email,
+     subject: "Registration Successful",
+     generateTextFromHTML: true,
+     html: "<h4> Hello "+firstName+" "+lastName+",</h4> <br> <p> Your Registration with AppointmentPlus was Successful.</p> <br> <p>Regards,<br>Abhinandan Walia<br>Team AppointmentPlus.</p>" 
+   };
+   smtpTransport.sendMail(mailOptions, (error, response) => {
+     error ? console.log(error) : console.log(response);
+     smtpTransport.close();
+   });}
+   sendEmail();
 
      return res.status(200).send({message:"success"});
      } 
